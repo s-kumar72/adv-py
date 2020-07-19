86dfe3c2ec1a8b55067d973037a813f5ac3bf434
s-kumar72 <kumarsadhana75@gmail.com>
2020-07-19 12:08:34 -0500

    tested frame in tkinter 7/18

--- testing.py
+++ testing.py
@@ -1,4 +1,18 @@
-#! /usr/bin/python
-print("hello world")
-print("hello world 2")
-print("hello world 3")
+
+import tkinter as tk
+
+window = tk.Tk()
+
+frame_a = tk.Frame()
+frame_b = tk.Frame()
+
+label_a = tk.Label(master=frame_a, text="I'm in Frame A")
+label_a.pack()
+
+label_b = tk.Label(master=frame_b, text="I'm in Frame B")
+label_b.pack()
+
+frame_a.pack()
+frame_b.pack()
+
+window.mainloop()
